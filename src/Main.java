7ab6ab331f2186c99af093c279cf4c37af1e520b
NAAGAVARSHINI <naagavarshinim@gmail.com>
2023-07-10 19:28:11 +0530

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+
+
+class Employee {
+    String name;
+    double salary;
+
+    double hourlyRate;
+    double hoursWorked;
+
+    public void calculatePay(double salary){
+        System.out.println("Name: " + name+ " Salary :" + salary);
+    }
+
+    public void calculatePay(double hourlyRate, double hoursWorked){
+        System.out.println("Name: " + name+ " Salary :" + hourlyRate*hoursWorked);
+    }
+}
+
+public class Main{
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Employee salariedEmployee = new Employee();
+        Employee hourlyEmployee= new Employee();
+        salariedEmployee.name = "Naveen";
+        salariedEmployee.salary=15000;
+        hourlyEmployee.name= "Varsha";
+        hourlyEmployee.hourlyRate = 200;
+        hourlyEmployee.hoursWorked= 10;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        salariedEmployee.calculatePay(salariedEmployee.salary);
+        hourlyEmployee.calculatePay(hourlyEmployee.hourlyRate, hourlyEmployee.hoursWorked);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+
     }
 }
